e50d42c10851203635e17bd24d3432ced6508b17
Tracey Le <traceyle@192-168-1-23.tpgi.com.au>
2017-03-01 22:28:09 +1100

    Uses project id for file naming instead
    This is done thanks to json.loads() :)

--- readUrl.py
+++ readUrl.py
@@ -22,7 +22,7 @@ def read_url(url_string):
 
 def write_json_file(data):
 
-    project_id = 621 #div_data['user_id']
+    project_id = json.loads(data)['uuid']
     file_path_project_data = os.path.join('project_data', str(project_id) + '.json')
 
     with open(file_path_project_data, 'w') as project_data_file:
